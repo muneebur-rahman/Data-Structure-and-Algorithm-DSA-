3be0ab73d2615bcb98c337c7c9d2670124e2b2d8
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-05-23 21:23:08 +0530

    Update

--- Two_Pointer_Pattern/Palindrome.java
+++ Two_Pointer_Pattern/Palindrome.java
@@ -3,13 +3,22 @@ package Two_Pointer_Pattern;
 // without using two pointer | (PCS) Comparision Searching Partitioning
 public class Palindrome {
     public static void main(String[] args) {
-        StringBuilder s=new StringBuilder("level");
-        StringBuilder s_rev=s.reverse();
-        
-        if(s==s_rev){
-            System.out.println(s+" is Palindrome");
-        }else{
-            System.out.println(s+" is Not Palindrome");
+        // StringBuilder s=new StringBuilder("level");
+        // StringBuilder s_rev=s.reverse();
+        String s = "leve";
+        String s_rev = new StringBuilder(s).reverse().toString();
+        int i = 0;
+
+        while (i < s.length()) {
+
+            if (s.charAt(i) != s_rev.charAt(i)) {
+                System.out.println(s + " is Not Palindrome");
+                return;
+            }
+            i += 1;
+            System.out.println(s + " is Not Palindrome");
+            break;
+
         }
 
     }
