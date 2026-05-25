a4124ec7bdb8f22eabb9a14ca9e1a1164ab33918
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-05-25 20:32:26 +0530

    Update LeetCode125.java

--- Two_Pointer_Pattern/LeetCode125.java
+++ Two_Pointer_Pattern/LeetCode125.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
 
 public class LeetCode125 {
     public static void main(String args[]){
-        
+        Scanner sc=new Scanner(System.in);
+        String s;
+        s=sc.nextLine();
+
+        int i=0,j=s.length()-1;
+
+        while(i<j){
+            char right=s.charAt(i);
+            char left=s.charAt(j);
+
+            if(!Character.isLetterOrDigit(right)){
+                i+=1;
+                continue;
+            }
+
+            if(!Character.isLetterOrDigit(left)){
+                left-=1;
+                continue;
+            }
+
+            if(Character.toLowerCase(right)!=Character.toLowerCase(left)){
+                System.out.println("Not Palindrome");
+            }
+            i++;
+            j--;
+        }
+        System.out.println("Palindrome");
+
+        sc.close();
     }
 }
